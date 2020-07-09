6cba4a63dd01da09624294a4b3d5333ac33902df
Jessica <duellje@oregonstate.edu>
2020-07-09 13:54:10 -0400

    completed lecture

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render,screen, getByTestId, getByText } from "@testing-library/react"; //render components in a virtual DOM
 import App from "./App";
 
+//jest global functions to write tests 
+// test() used to run a test 
+//Jest is going to be the test runner (command line interface)
 
+// test('title', cbToRunTheTest)
+test('renders App component without errors', () => {
+    render(<App />);
+
+    //tests will fail if an error is thrown from any function or component inside the test callback 
+
+})
+
+//query the virtual dom for an element and make an "assertion" about that element
+test('App renders the form header', () => {
+    //Arrange
+    render(<App />);
+
+    //Act
+    //query for the header element
+    // regex string with i following to get case insensitive 
+    const header = screen.getByText(/add new animal/i)
+
+    //Assert 
+    //make an assertion
+    expect(header).toBeInTheDocument()
+})
--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -1,39 +1,40 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render,screen, fireEvent } from "@testing-library/react";
 import AnimalForm from "./AnimalForm";
 
-test("renders correctly", () => {
-  render(<AnimalForm />);
-});
+test('AnimalForm adds new animals to the list', ()=>{
+    render(<AnimalForm/>);
+    //type into all three inputs 
+    /*
+        1. query for all inputs
+        2. run the change event to add text
+    */
+    const speciesInput = screen.getByLabelText(/species/i);
+    const ageInput = screen.getByLabelText(/age/i);
+    const notesInput = screen.getByLabelText(/notes/i);
 
-test("animal form adds new animals to animal list", () => {
-  const { getByLabelText, getByText, findAllByText } = render(<AnimalForm />);
+    //events with react testing library come with fireEvent
+    fireEvent.change(speciesInput, {target:{value: 'Tiger'}});
+    fireEvent.change(ageInput, {target:{value: 13}});
+    fireEvent.change(notesInput, {target:{value:'Friendly'}});
 
-  // query for the form inputs
-  const speciesInput = getByLabelText(/species/i);
-  const ageInput = getByLabelText(/age/i);
-  const notesInput = getByLabelText(/notes/i);
 
-  // fireEvent function from RTL to fill in the inputs
-  fireEvent.change(speciesInput, {
-    target: { name: "species", value: "Elephant" }
-  });
-  fireEvent.change(ageInput, {
-    target: { name: "age", value: "28" }
-  });
-  fireEvent.change(notesInput, {
-    target: { name: "notes", value: "Really old, but very sweet" }
-  });
+    //click submit button
+    /*
+        1. query for the button
+        2. run the click event on the button
+     */
+    const submitButton = screen.getByText(/submit/i);
+    fireEvent.click(submitButton);
 
-  console.log(speciesInput.value);
+    //assert that the new animal is in the list 
+    /*
+        1. query for the new animal text
+        2. assert that it is being rendered 
+    */
+    const newTiger = screen.getByText(/tiger/i);
+    expect(newTiger).toBeInTheDocument();
 
-  // query for the submit button
-  const submitButton = getByText(/submit!/i);
+    //could also do screen.getByText(/tiger/i) since it will fail if the text is not in the document so you don't need the full assertion if you don't want it
 
-  // clicking the button
-  fireEvent.click(submitButton);
-  fireEvent.click(submitButton);
-
-  // assertion
-  findAllByText(/elephant/i);
-});
+})
